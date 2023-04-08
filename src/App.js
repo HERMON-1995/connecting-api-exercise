ff2c77c23c0291be97a860d2f71135f70983b066
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-08 16:23:28 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UsersList from './components/displayUsers'
+
+function App() {
+  return (
+    <div>
+      <h1 className="App">User Names</h1>
+      <UsersList />
+    </div>
+  );
+}
+
+export default App;
